860d46e9cd340451485d463b0cc80437662528b5
Sharma30042000 <sagarsharma3042@gmail.com>
2021-07-28 09:58:24 +0530

    ok

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 
 from app.webhook.routes import webhook
-
+from app.extensions import mongo
 
 # Creating our flask app
 def create_app():
 
     app = Flask(__name__)
     
+
+    app.config["MONGO_URI"]="mongodb://localhost:27017/election"
+    with app.app_context():
+        mongo.init_app(app)
     # registering all the blueprints
     app.register_blueprint(webhook)
-    
     return app
--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, json, request
-
+from flask import Blueprint, json, request,render_template
+from app.extensions import mongo
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
+
+
+@webhook.route('/',methods=['GET','POST'])
+def home():
+    male = mongo.db.contacts.find({"gender": 'male'})
+    return render_template('home.html',data=male) 
 
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
