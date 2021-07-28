e4448dbcf718bd835aae739a8715f437c3ad353c
Sharma30042000 <sagarsharma3042@gmail.com>
2021-07-28 11:23:07 +0530

    ok final

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -12,7 +12,7 @@ def home():
 def receiver():
     if request.headers['Content-Type'] == 'application/json':
         data=json.dumps(request.json)
-        print("ok again")
-        
+        print("ok final")
+
         print(data)
         return render_template('home.html',data=data)
